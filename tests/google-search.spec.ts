5c3c7e309c2449421835a19a4fe3f318356a2c93
TestologyAutomation <testology.qa.learning@gmail.com>
2026-03-08 11:25:10 +0530

    Initial Playwright demo project

---
+++ tests/google-search.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Google search demo', async ({ page }) => {
+
+  // Open Google
+  await page.goto('https://www.google.com');
+
+  // Type search query
+  await page.fill('[name="q"]', 'Playwright Test automation by NiteshJain QaTestology');
+
+  // Press Enter
+  await page.keyboard.press('Enter');
+
+  // Verify results page loads
+  await expect(page).toHaveTitle(/Playwright/);
+
+});
